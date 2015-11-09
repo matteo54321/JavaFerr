85a9a393198996751b67f84da78e86fc141371e5
Dago86 <diego.deangelis@studio.unibo.it>
2015-11-09 14:32:58 +0100

    Update L4E3.java

--- L4E3.java
+++ L4E3.java
@@ -1,56 +1,72 @@
 import java.util.Scanner;
 public class L4E3{
-public static void main(String[] args){
-Scanner in = new Scanner (System.in);
-int[] ArrayUno = new int[10];
-int[] ArrayDue = new int[10];
-
-for (int i=0; i<10; i++){
-System.out.println("Inserire valori pari");
-ArrayUno[i] = in.nextInt();
-}
-
-for (int i=0; i<10; i++){
-System.out.println("Inserire valori dispari");
-ArrayDue[i] = in.nextInt();
-}
-
-int [] ArrayFin = new int[ArrayUno.length + ArrayDue.length];
-
- byte index1=0;
- byte index2=0;
- boolean array1Terminato=false;
- boolean array2Terminato=false;
-
- for (byte index=0; index<ArrayFin.length; index++)
- {
-  if ((ArrayUno[index1]<=ArrayDue[index2] || array2Terminato) && !array1Terminato)
-  {
-   ArrayFin[index]=ArrayUno[index1];
-   if (index1<ArrayUno.length-1)
-    index1++;
-   else
-    array1Terminato=true;
-  }
-  else
-  {
-   ArrayFin[index]=ArrayDue[index2];
-   if (index2<ArrayDue.length-1)
-    index2++;
-   else
-    array2Terminato=true;
-  }
- }
- 
- for (byte index=0; index<ArrayFin.length; index++)
-  System.out.println(ArrayFin[index]);
-
-
-
-
-
-
-
-
-}
+	public static void main(String[] args){
+		Scanner in = new Scanner (System.in);
+		int[] ArrayUno = new int[10];
+		int[] ArrayDue = new int[10];
+		
+		for (int i=0; i<10; i++){
+			System.out.println("Inserire valori pari");
+			ArrayUno[i] = in.nextInt();
+		}
+		
+		for (int i=0; i<10; i++){
+			System.out.println("Inserire valori dispari");
+			ArrayDue[i] = in.nextInt();
+		}
+		
+		int [] ArrayFin = new int[ArrayUno.length + ArrayDue.length];
+		
+		byte index1=0;
+		byte index2=0;
+		boolean array1Terminato=false;
+		boolean array2Terminato=false;
+		
+		for (byte index=0; index<ArrayFin.length; index++)
+		{
+			if ((ArrayUno[index1]<=ArrayDue[index2] || array2Terminato) && !array1Terminato)
+			{
+				ArrayFin[index]=ArrayUno[index1];
+				if (index1<ArrayUno.length-1)
+					index1++;
+				else
+					array1Terminato=true;
+			}
+			else
+			{
+				ArrayFin[index]=ArrayDue[index2];
+				if (index2<ArrayDue.length-1)
+					index2++;
+				else
+					array2Terminato=true;
+			}
+		}
+		
+		
+		for(int i = 0; i < ArrayFin.length-1; i++) {
+			int minimo = i; //Partiamo dall' i-esimo elemento
+			for(int j = i+1; j < ArrayFin.length; j++) {
+				if(ArrayFin[minimo]>ArrayFin[j]) {
+					minimo = j;
+				}
+			}
+			
+			
+			if(minimo!=i) { 
+				int k = ArrayFin[minimo];
+				ArrayFin[minimo]= ArrayFin[i];
+				ArrayFin[i] = k;
+			}
+		}
+		
+		for (byte index=0; index<ArrayFin.length; index++)
+			System.out.println(ArrayFin[index]);
+		
+		
+		
+		
+		
+		
+		
+	}
 }
